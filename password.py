e1f177d6e5ad1c04e74e782b0687a01f22b65e68
ChanYvonne <ychan1352@gmail.com>
2017-04-23 15:53:51 -0400

    debugged and finished part one

--- password.py
+++ password.py
@@ -1,14 +1,22 @@
 #WORKING
 def hasRequirements(password):
-    hasLower = [char for char in list(password) if char == char.lower() ]
-    
-    hasUpper = [char for char in list(password) if char == char.upper() ]
+    hasLower = [char for char in list(password) if char == char.lower() and not char.isdigit() ]
+    print hasLower
+    hasUpper = [char for char in list(password) if char == char.upper() and not char.isdigit() ]
+    print hasUpper
     hasNumber = [char for char in list(password) if char.isdigit() and int(char) in range(0, 10)]  
-    
+    print hasNumber
     if (hasLower and hasUpper and hasNumber):
         return True
     else:
         return False
+
+#testing
+print hasRequirements("PASSWORRDDD1")
+print hasRequirements("yayforfreeperiods2")
+print hasRequirements("softdev")
+print hasRequirements("Ineedfood123")
+
 
 #max strength is 5
 def passwordStrength(password):
